b999b624e7b5d841515b4fca0eca4e3a09253fc4
mainsail <mainsail@fedora41.mainsail.lab>
2025-03-09 16:44:44 -0400

    removed some pkgs 2

--- build.sh
+++ build.sh
@@ -11,17 +11,15 @@ METALVISOR_PACKAGES=(
     openvswitch
     nats-server.x86_64
 )
+# golang-github-nats-io-devel.noarch \
+# golang-github-nats-io-jwt-2.x86_64 \
+# golang-github-nats-io-jwt-devel.noarch \
+# golang-github-nats-io-nkeys.x86_64 \
+# golang-github-nats-io-nkeys-devel.noarch \
+# golang-github-nats-io-server-devel.noarch \
+# golang-github-nats-io-streaming-server.x86_64 \
+# golang-github-nats-io-streaming-server-devel.noarch 
 
-    # golang-github-nats-io-devel.noarch \
-    # golang-github-nats-io-jwt-2.x86_64 \
-    # golang-github-nats-io-jwt-devel.noarch \
-    # golang-github-nats-io-nkeys.x86_64 \
-    # golang-github-nats-io-nkeys-devel.noarch \
-    # golang-github-nats-io-server-devel.noarch \
-    # golang-github-nats-io-streaming-server.x86_64 \
-    # golang-github-nats-io-streaming-server-devel.noarch 
-
-# Install Metalvisor-related packages
 dnf5 install -y "${METALVISOR_PACKAGES[@]}"
 
 echo "::endgroup::"
